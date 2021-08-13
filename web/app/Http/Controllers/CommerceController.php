957dfe339b06b34d14a0cf27107bd1a2ac6d750a
DerianCordobaPerez <derianunanleon@gmail.com>
2021-08-12 23:39:05 -0600

    Creacion de controladores para los modelos mas la creacion del controlador Home con el cual mostraremos las vistas generales de la pagina

---
+++ web/app/Http/Controllers/CommerceController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Commerce;
+use Illuminate\Http\Request;
+
+class CommerceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Commerce  $commerce
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Commerce $commerce)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Commerce  $commerce
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Commerce $commerce)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Commerce  $commerce
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Commerce $commerce)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Commerce  $commerce
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Commerce $commerce)
+    {
+        //
+    }
+}
