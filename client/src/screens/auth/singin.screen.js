052472a2bdc026573540d87452ad30ddfe9c2ba6
DerianCordobaPerez <derianunanleon@gmail.com>
2021-08-12 18:52:37 -0600

    Se ha cambiado el texto dentro de la pantalla de signin

--- client/src/screens/auth/singin.screen.js
+++ client/src/screens/auth/singin.screen.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { MaterialCommunityIcons, Ionicons } from '@expo/vector-icons';
+import {MaterialCommunityIcons} from '@expo/vector-icons'
 import {
   NativeBaseProvider,
   Box,
@@ -13,92 +13,95 @@ import {
   Icon,
   IconButton,
   HStack,
-  Center
-} from 'native-base';
+  Center,
+} from 'native-base'
 
 export const SingInScreen = () => (
-    <NativeBaseProvider>
-      <Box
-        safeArea
-        flex={1}
-        p={2}
-        w="90%"
-        mx='auto'
-      >
-        <Center marginTop={"40"}>
-        <Heading size="xl" color='primary.500'>
-          Welcome
+  <NativeBaseProvider>
+    <Box safeArea flex={1} p={2} w="90%" mx="auto">
+      <Center marginTop={'40'}>
+        <Heading size="xl" color="primary.500">
+          Bienvenido
         </Heading>
         <Heading color="muted.400" size="lg">
-          Sign in to continue!
+          Inicia sesion para continuar
         </Heading>
-        </Center>
+      </Center>
 
-        <VStack space={2} mt={5}>
-          <FormControl>
-            <FormControl.Label _text={{color: 'muted.700', fontSize: 'sm', fontWeight: 600}}>
-                Email ID
-            </FormControl.Label>
-            <Input />
-          </FormControl>
-          <FormControl mb={5}>
-            <FormControl.Label  _text={{color: 'muted.700', fontSize: 'sm', fontWeight: 600}}>
-                Password
-            </FormControl.Label>
-            <Input type="password" />
-            <Link
-              _text={{ fontSize: 'xs', fontWeight: '700', color:'cyan.500' }}
-              alignSelf="flex-end"
-              mt={1}
-            >
-              Forget Password?
-            </Link>
-          </FormControl>
-          <VStack  space={2}>
-          <Button colorScheme="cyan" _text={{color: 'white' }}>
-              Login
+      <VStack space={2} mt={5}>
+        <FormControl>
+          <FormControl.Label
+            _text={{color: 'muted.700', fontSize: 'sm', fontWeight: 600}}
+          >
+            Email
+          </FormControl.Label>
+          <Input />
+        </FormControl>
+        <FormControl mb={5}>
+          <FormControl.Label
+            _text={{color: 'muted.700', fontSize: 'sm', fontWeight: 600}}
+          >
+            Password
+          </FormControl.Label>
+          <Input type="password" />
+          <Link
+            _text={{fontSize: 'xs', fontWeight: '700', color: 'cyan.500'}}
+            alignSelf="flex-end"
+            mt={1}
+          >
+            Restaurar Password?
+          </Link>
+        </FormControl>
+        <VStack space={2}>
+          <Button colorScheme="cyan" _text={{color: 'white'}}>
+            Login
           </Button>
 
-<HStack justifyContent="center" alignItem='center'>
-          <IconButton
-            variant='unstyled'
-            startIcon={
-              <Icon
-                as={< MaterialCommunityIcons name="facebook" />}
-                color='muted.700'
-                size='sm'
-              />
-            }
-          />
-          <IconButton
-            variant='unstyled'
-            startIcon={
-              <Icon
-                as={< MaterialCommunityIcons name="google" />}
-                color='muted.700'
-                size="sm"
-              />
-            }
-          />
-          <IconButton
-            variant='unstyled'
-            startIcon={
-              <Icon
-                as={< MaterialCommunityIcons name="github" />}
-                color='muted.700'
-                size="sm"
-              />
-            }
-          />
-          </HStack>
-          </VStack>
-          <HStack justifyContent="center">
-            <Text fontSize='sm' color='muted.700' fontWeight={400}>I'm a new user. </Text>
-            <Link _text={{ color: 'cyan.500', bold: true, fontSize: 'sm' }} href="#">
-              Sign Up
-            </Link>
+          <HStack justifyContent="center" alignItem="center">
+            <IconButton
+              variant="unstyled"
+              startIcon={
+                <Icon
+                  as={<MaterialCommunityIcons name="facebook" />}
+                  color="muted.700"
+                  size="sm"
+                />
+              }
+            />
+            <IconButton
+              variant="unstyled"
+              startIcon={
+                <Icon
+                  as={<MaterialCommunityIcons name="google" />}
+                  color="muted.700"
+                  size="sm"
+                />
+              }
+            />
+            <IconButton
+              variant="unstyled"
+              startIcon={
+                <Icon
+                  as={<MaterialCommunityIcons name="github" />}
+                  color="muted.700"
+                  size="sm"
+                />
+              }
+            />
           </HStack>
         </VStack>
-      </Box>
-    </NativeBaseProvider>
+        <HStack justifyContent="center">
+          <Text fontSize="sm" color="muted.700" fontWeight={400}>
+            No tienes una cuenta?.{' '}
+          </Text>
+          <Link
+            _text={{color: 'cyan.500', bold: true, fontSize: 'sm'}}
+            href="#"
+          >
+            Registrarse
+          </Link>
+        </HStack>
+      </VStack>
+    </Box>
+  </NativeBaseProvider>
 )
