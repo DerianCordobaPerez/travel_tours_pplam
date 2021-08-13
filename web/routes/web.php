1270e6490d697338d3054ca54b9e5c644904a1e7
DerianCordobaPerez <derianunanleon@gmail.com>
2021-08-12 23:44:50 -0600

    Rutas para las vistas generales

--- web/routes/web.php
+++ web/routes/web.php
@@ -1,18 +1,17 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+// Rutas autenticacion
+Auth::routes();
 
-Route::get('/', function () {
-    return view('welcome');
+// Rutas cliente
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/about', [HomeController::class, 'about'])->name('about');
+
+// Rutas mapa
+Route::group(['middleware' => 'auth'], function() {
+    
 });
